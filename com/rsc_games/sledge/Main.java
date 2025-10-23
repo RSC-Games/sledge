4f54bc6cd0410a18208f0f11b30928a83d8813ad
RSC Games <rsc.games.official@gmail.com>
2025-10-23 14:38:16 -0400

    chore: license changed to prohibit training of AI models with this codebase
    Relicensed to Apache NON-AI License. All previous commits within this specific
    canonical repository are also retroactively relicensed under this new license.

--- com/rsc_games/sledge/Main.java
+++ com/rsc_games/sledge/Main.java
@@ -1,7 +1,7 @@
 /**
  * Copyright 2024-2025 RSC Games, Raine Bond
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache NON-AI License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
